cf05bb1e3fc50fdaf3c924ff8f371f53560e6b82
Amna5455 <syedaamna637@gmail.com>
2023-10-09 14:14:41 +0500

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         {{--  <link href="{{ asset('css/chat.css') }}" rel="stylesheet">  --}}
         @vite(['resources/css/app.css', 'resources/js/app.js','resources/css/sass/chat.scss'])
+
     </head>
     <body >
         <div class="container">
